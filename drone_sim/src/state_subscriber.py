f7127b3ddbdb00b1efded531cdc943af7fbeafad
cm7799 <7799cm@gmail.com>
2019-10-11 02:06:46 +0530

    initialised

---
+++ drone_sim/src/state_subscriber.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python
+
+import rospy
+from mavros_msgs.msg import State
+
+def callback(data):
+  rospy.loginfo(data.mode)
+
+
+
+def mavros_state_subscriber():
+   rospy.init_node('state_subscriber',anonymous=False)
+   rospy.Subscriber("/mavros/state",State,callback)
+   rospy.spin()
+
+
+
+
+
+if __name__ == '__main__':
+  while not rospy.is_shutdown():
+    mavros_state_subscriber()
